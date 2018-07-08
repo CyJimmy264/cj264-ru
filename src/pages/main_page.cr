b83e553ba230468145229a37e6cc14f38ded7e1e
mveynberg <mv@cj264.ru>
2018-07-08 20:19:19 +0500

    Add a-ads ad_unit

--- src/pages/main_page.cr
+++ src/pages/main_page.cr
@@ -7,5 +7,9 @@ class MainPage < MainLayout
     main class: "container", role: "main" do
       text "Hallelujah!"
     end
+
+    div style: "text-align: center; position: absolute; bottom: 0; padding: 0 auto; width: 100%;" do
+      raw "<iframe data-aa='870016' src='//ad.a-ads.com/870016?size=336x280' scrolling='no' style='width:336px; height:280px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"
+    end
   end
 end
